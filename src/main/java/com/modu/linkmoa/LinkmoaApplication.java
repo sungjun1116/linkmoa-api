9037210cb543ab3019094c55b15cd8a09f3be9c2
황성준 <opr99@naver.com>
2021-09-13 22:16:38 +0900

    Initialize Project

---
+++ src/main/java/com/modu/linkmoa/LinkmoaApplication.java
@@ -0,0 +1,13 @@
+package com.modu.linkmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modu/linkmoa/LinkmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modu.linkmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
